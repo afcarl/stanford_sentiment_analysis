829f144ff4e37d1b4b2fc7f81ed399351bf4c309
Kazi Nazmul Haque Shezan <shezan.huq@gmail.com>
2016-09-08 15:56:28 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ fi
 #Checking the existence of the file
 if [ ! -f "$HOME/$DIRECTORY/$FILENAME" ]
 then
+    echo 'Please wait I need to download around 400 MB. of file'
     wget -O $HOME/$DIRECTORY/stanford-corenlp-full-2015-12-09.zip http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip
 else
 	#Checking the file size
 	FILESIZE=$(wc -c <"$HOME/$DIRECTORY/$FILENAME")
 	
-	echo $FILESIZE
-	
 	if [ $FILESIZE -lt 403157240 ]; then
+		echo 'Last time the download was interrupted so downloading the rest of 400 MB. '
     	        wget -O $HOME/$DIRECTORY/stanford-corenlp-full-2015-12-09.zip http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip
 	fi
 fi
@@ -31,3 +31,4 @@ fi
 
 cd stanford_sentiment
 sudo python setup.py install
+echo "Awesome! Please enjoy the package"
